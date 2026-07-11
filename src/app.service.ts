f402e2a835ef40fe8f9959c028ce74b59d3648d0
Sencoool <jiramet.sakulkitthavorn@gmail.com>
2026-07-11 23:14:00 +0700

    fix: unit test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello the server is online, Gawston!';
+    return 'Hello World!';
+  }
+
+  getGawston(): string {
+    return 'Hello Gawston!'
   }
 }
